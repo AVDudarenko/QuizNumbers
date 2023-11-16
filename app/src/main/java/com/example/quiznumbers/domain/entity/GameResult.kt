3216e10650739ed58de64ac08526b6257d6b5426
aliaksandr <aleksandr.dudarenko@gmail.com>
2023-11-16 15:11:13 +0300

    add parcelable

--- app/src/main/java/com/example/quiznumbers/domain/entity/GameResult.kt
+++ app/src/main/java/com/example/quiznumbers/domain/entity/GameResult.kt
@@ -1,10 +1,13 @@
 package com.example.quiznumbers.domain.entity
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 import java.io.Serializable
 
+@Parcelize
 data class GameResult(
     val isWinner: Boolean,
     val countOfRightAnswers: Int,
     val countOFQuestions: Int,
     val gameSettings: GameSettings
-) : Serializable
+) : Parcelable
