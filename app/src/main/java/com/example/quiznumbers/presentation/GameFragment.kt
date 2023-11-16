05862114946274541df401c5037c88bbd7e4a6a8
aliaksandr <aleksandr.dudarenko@gmail.com>
2023-11-16 13:16:38 +0300

    add view binding

--- app/src/main/java/com/example/quiznumbers/presentation/GameFragment.kt
+++ app/src/main/java/com/example/quiznumbers/presentation/GameFragment.kt
@@ -1,17 +1,34 @@
 package com.example.quiznumbers.presentation
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.quiznumbers.R
+import androidx.fragment.app.Fragment
+import com.example.quiznumbers.databinding.FragmentGameBinding
 
 class GameFragment : Fragment() {
+
+    private var _binding: FragmentGameBinding? = null
+    private val binding: FragmentGameBinding
+        get() = _binding ?: throw RuntimeException("GameFragmentBinding == null")
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_game, container, false)
+    ): View {
+        _binding = FragmentGameBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.tvOption1
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
