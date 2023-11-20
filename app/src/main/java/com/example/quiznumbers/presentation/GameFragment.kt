2deb9ef94b21ec89ab20858302acd99d2ca0d281
Aliaksandr <dyvedd@gmail.com>
2023-11-20 13:16:33 +0300

    add view model

--- app/src/main/java/com/example/quiznumbers/presentation/GameFragment.kt
+++ app/src/main/java/com/example/quiznumbers/presentation/GameFragment.kt
@@ -5,63 +5,70 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.example.quiznumbers.R
 import com.example.quiznumbers.databinding.FragmentGameBinding
 import com.example.quiznumbers.domain.entity.GameResult
 import com.example.quiznumbers.domain.entity.Level
 
 class GameFragment : Fragment() {
-    private lateinit var level: Level
+	private lateinit var level: Level
+	private val viewModel: GameViewModel by lazy {
+		ViewModelProvider(
+			this,
+			ViewModelProvider.AndroidViewModelFactory.getInstance(requireActivity().application)
+		)[GameViewModel::class.java]
+	}
 
-    private var _binding: FragmentGameBinding? = null
-    private val binding: FragmentGameBinding
-        get() = _binding ?: throw RuntimeException("GameFragmentBinding == null")
+	private var _binding: FragmentGameBinding? = null
+	private val binding: FragmentGameBinding
+		get() = _binding ?: throw RuntimeException("GameFragmentBinding == null")
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        parseArgs()
-    }
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		parseArgs()
+	}
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        _binding = FragmentGameBinding.inflate(inflater, container, false)
-        return binding.root
-    }
+	override fun onCreateView(
+		inflater: LayoutInflater, container: ViewGroup?,
+		savedInstanceState: Bundle?
+	): View {
+		_binding = FragmentGameBinding.inflate(inflater, container, false)
+		return binding.root
+	}
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        binding.tvOption1
-    }
+	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+		super.onViewCreated(view, savedInstanceState)
+		binding.tvOption1
+	}
 
-    private fun launchGameFinishedFragment(gameResult: GameResult) {
-        requireActivity().supportFragmentManager.beginTransaction()
-            .replace(R.id.main_container, GameFinishedFragment.newInstance(gameResult))
-            .addToBackStack(null)
-            .commit()
-    }
+	private fun launchGameFinishedFragment(gameResult: GameResult) {
+		requireActivity().supportFragmentManager.beginTransaction()
+			.replace(R.id.main_container, GameFinishedFragment.newInstance(gameResult))
+			.addToBackStack(null)
+			.commit()
+	}
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
+	override fun onDestroyView() {
+		super.onDestroyView()
+		_binding = null
+	}
 
-    private fun parseArgs() {
-        requireArguments().getParcelable<Level>(KEY_LEVEL)?.let {
-            level = it
-        }
-    }
+	private fun parseArgs() {
+		requireArguments().getParcelable<Level>(KEY_LEVEL)?.let {
+			level = it
+		}
+	}
 
-    companion object {
-        const val FRAGMENT_NAME = "GameFinishedFragment"
-        private const val KEY_LEVEL = "level"
-        fun newInstance(level: Level): GameFragment {
-            return GameFragment().apply {
-                arguments = Bundle().apply {
-                    putParcelable(KEY_LEVEL, level)
-                }
-            }
-        }
-    }
+	companion object {
+		const val FRAGMENT_NAME = "GameFinishedFragment"
+		private const val KEY_LEVEL = "level"
+		fun newInstance(level: Level): GameFragment {
+			return GameFragment().apply {
+				arguments = Bundle().apply {
+					putParcelable(KEY_LEVEL, level)
+				}
+			}
+		}
+	}
 }
